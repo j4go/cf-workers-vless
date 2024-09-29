308c9e1919f1033eb2711b64ec469272ae485a15
j4 <yianny@163.com>
2024-09-29 15:05:24 +0800

    test zlib

--- _worker.js
+++ _worker.js
@@ -1,138 +1,199 @@
-// _worker.js
-
-// 反代目标网站
-const upstream = 'ipv6.google.com.hk'
-const upstream_v4 = 'www.google.com.hk'
-
-// 访问区域黑名单（按需设置）.
-const blocked_region = []
-
-//资源重定向
-const replace_dict = {
-  $upstream: '$custom_domain',
-  'www.google.com/': 'cfpage-4testing.pages.dev/', //填入你的子域名
-  'gstatic.com': 'gstatic.cn',
-  'ajax.googleapis.com': 'ajax.lug.ustc.edu.cn',
-  'fonts.googleapis.com': 'fonts.googleapis.cn',
-  'themes.googleusercontent.com': 'google-themes.lug.ustc.edu.cn',
-  'www.gravatar.com/avatar': 'dn-qiniu-avatar.qbox.me/avatar',
-  'www.google.co.jp': '$custom_domain',
-  'www.google.com.sg': '$custom_domain',
-  'books.google.com.hk': '$custom_domain',
-  'books.google.co.jp': '$custom_domain',
-  'books.google.com.sg': '$custom_domain',
-  'maps.google.com.hk': '$custom_domain',
-  'maps.google.co.jp': '$custom_domain',
-  'maps.google.com.sg': '$custom_domain',
-  'maps.google.com': '$custom_domain',
-  'books.google.com': '$custom_domain',
+function logError(request, message) {
+  console.error(
+    `${message}, clientIp: ${request.headers.get(
+      "cf-connecting-ip"
+    )}, user-agent: ${request.headers.get("user-agent")}, url: ${request.url}`
+  );
 }
 
-addEventListener('fetch', (event) => {
-  event.respondWith(fetchAndApply(event.request))
-})
-
-async function fetchAndApply(request) {
-  const region = request.headers.get('cf-ipcountry').toUpperCase()
-//   const ip_address = request.headers.get('cf-connecting-ip')
-//   const user_agent = request.headers.get('user-agent')
-
-  let response = null
-  let url = new URL(request.url)
-  let url_host = url.host
-
-  if (url.protocol == 'http:') {
-    url.protocol = 'https:'
-    response = Response.redirect(url.href)
-    return response
+function createNewRequest(request, url, proxyHostname, originHostname) {
+  const newRequestHeaders = new Headers(request.headers);
+  for (const [key, value] of newRequestHeaders) {
+    if (value.includes(originHostname)) {
+      newRequestHeaders.set(
+        key,
+        value.replace(
+          new RegExp(`(?<!\\.)\\b${originHostname}\\b`, "g"),
+          proxyHostname
+        )
+      );
+    }
   }
+  return new Request(url.toString(), {
+    method: request.method,
+    headers: newRequestHeaders,
+    body: request.body,
+  });
+}
 
-  //检查是否为图片搜索
-  var key = url.href
-  var ikey1 = 'tbm=isch'
-  var ikey2 = '/img'
-  if ((key.search(ikey1) == -1) && (key.search(ikey2) == -1)) {
-    var upstream_domain = upstream
+function setResponseHeaders(
+  originalResponse,
+  proxyHostname,
+  originHostname,
+  DEBUG
+) {
+  const newResponseHeaders = new Headers(originalResponse.headers);
+  for (const [key, value] of newResponseHeaders) {
+    if (value.includes(proxyHostname)) {
+      newResponseHeaders.set(
+        key,
+        value.replace(
+          new RegExp(`(?<!\\.)\\b${proxyHostname}\\b`, "g"),
+          originHostname
+        )
+      );
+    }
+  }
+  if (DEBUG) {
+    newResponseHeaders.delete("content-security-policy");
+  }
+  return newResponseHeaders;
+}
+
+/**
+ * 替换内容
+ * @param originalResponse 响应
+ * @param proxyHostname 代理地址 hostname
+ * @param pathnameRegex 代理地址路径匹配的正则表达式
+ * @param originHostname 替换的字符串
+ * @returns {Promise<*>}
+ */
+async function replaceResponseText(
+  originalResponse,
+  proxyHostname,
+  pathnameRegex,
+  originHostname
+) {
+  let text = await originalResponse.text();
+  if (pathnameRegex) {
+    pathnameRegex = pathnameRegex.replace(/^\^/, "");
+    return text.replace(
+      new RegExp(`((?<!\\.)\\b${proxyHostname}\\b)(${pathnameRegex})`, "g"),
+      `${originHostname}$2`
+    );
   } else {
-    var upstream_domain = upstream_v4
+    return text.replace(
+      new RegExp(`(?<!\\.)\\b${proxyHostname}\\b`, "g"),
+      originHostname
+    );
   }
+}
 
-  url.host = upstream_domain
+async function nginx() {
+  return `<!DOCTYPE html>
+<html>
+<head>
+<title>Welcome to nginx!</title>
+<style>
+html { color-scheme: light dark; }
+body { width: 35em; margin: 0 auto;
+font-family: Tahoma, Verdana, Arial, sans-serif; }
+</style>
+</head>
+<body>
+<h1>Welcome to nginx!</h1>
+<p>If you see this page, the nginx web server is successfully installed and
+working. Further configuration is required.</p>
 
-  if (blocked_region.includes(region)) {
-    response = new Response(
-      'Access denied: WorkersProxy is not available in your region yet.',
-      {
-        status: 403,
+<p>For online documentation and support please refer to
+<a href="http://nginx.org/">nginx.org</a>.<br/>
+Commercial support is available at
+<a href="http://nginx.com/">nginx.com</a>.</p>
+
+<p><em>Thank you for using nginx.</em></p>
+</body>
+</html>`;
+}
+
+export default {
+  async fetch(request, env, ctx) {
+    try {
+      const {
+        PROXY_HOSTNAME = "z-lib.gs",
+        PROXY_PROTOCOL = "https",
+        PATHNAME_REGEX,
+        UA_WHITELIST_REGEX,
+        UA_BLACKLIST_REGEX,
+        URL302,
+        IP_WHITELIST_REGEX,
+        IP_BLACKLIST_REGEX,
+        REGION_WHITELIST_REGEX,
+        REGION_BLACKLIST_REGEX,
+        DEBUG = false,
+      } = env;
+      const url = new URL(request.url);
+      const originHostname = url.hostname;
+      if (
+        !PROXY_HOSTNAME ||
+        (PATHNAME_REGEX && !new RegExp(PATHNAME_REGEX).test(url.pathname)) ||
+        (UA_WHITELIST_REGEX &&
+          !new RegExp(UA_WHITELIST_REGEX).test(
+            request.headers.get("user-agent").toLowerCase()
+          )) ||
+        (UA_BLACKLIST_REGEX &&
+          new RegExp(UA_BLACKLIST_REGEX).test(
+            request.headers.get("user-agent").toLowerCase()
+          )) ||
+        (IP_WHITELIST_REGEX &&
+          !new RegExp(IP_WHITELIST_REGEX).test(
+            request.headers.get("cf-connecting-ip")
+          )) ||
+        (IP_BLACKLIST_REGEX &&
+          new RegExp(IP_BLACKLIST_REGEX).test(
+            request.headers.get("cf-connecting-ip")
+          )) ||
+        (REGION_WHITELIST_REGEX &&
+          !new RegExp(REGION_WHITELIST_REGEX).test(
+            request.headers.get("cf-ipcountry")
+          )) ||
+        (REGION_BLACKLIST_REGEX &&
+          new RegExp(REGION_BLACKLIST_REGEX).test(
+            request.headers.get("cf-ipcountry")
+          ))
+      ) {
+        logError(request, "Invalid");
+        return URL302
+          ? Response.redirect(URL302, 302)
+          : new Response(await nginx(), {
+              headers: {
+                "Content-Type": "text/html; charset=utf-8",
+              },
+            });
       }
-    )
-  } else {
-    let method = request.method
-    let request_headers = request.headers
-    let new_request_headers = new Headers(request_headers)
-
-    new_request_headers.set('Host', upstream_domain)
-    new_request_headers.set('Referer', url.href)
-
-    let original_response = await fetch(url.href, {
-      method: method,
-      headers: new_request_headers,
-    })
-
-    let original_response_clone = original_response.clone()
-    let original_text = null
-    let response_headers = original_response.headers
-    let new_response_headers = new Headers(response_headers)
-    let status = original_response.status
-
-    new_response_headers.set('cache-control', 'public, max-age=14400')
-    new_response_headers.set('access-control-allow-origin', '*')
-    new_response_headers.set('access-control-allow-credentials', true)
-    new_response_headers.delete('content-security-policy')
-    new_response_headers.delete('content-security-policy-report-only')
-    new_response_headers.delete('clear-site-data')
-
-    const content_type = new_response_headers.get('content-type')
-    if (content_type.includes('text/html') && content_type.includes('UTF-8')) {
-      // && content_type.includes('UTF-8')
-      original_text = await replace_response_text(
-        original_response_clone,
-        upstream_domain,
-        url_host
-      )
-    } else {
-      original_text = original_response_clone.body
+      url.host = PROXY_HOSTNAME;
+      url.protocol = PROXY_PROTOCOL;
+      const newRequest = createNewRequest(
+        request,
+        url,
+        PROXY_HOSTNAME,
+        originHostname
+      );
+      const originalResponse = await fetch(newRequest);
+      const newResponseHeaders = setResponseHeaders(
+        originalResponse,
+        PROXY_HOSTNAME,
+        originHostname,
+        DEBUG
+      );
+      const contentType = newResponseHeaders.get("content-type") || "";
+      let body;
+      if (contentType.includes("text/")) {
+        body = await replaceResponseText(
+          originalResponse,
+          PROXY_HOSTNAME,
+          PATHNAME_REGEX,
+          originHostname
+        );
+      } else {
+        body = originalResponse.body;
+      }
+      return new Response(body, {
+        status: originalResponse.status,
+        headers: newResponseHeaders,
+      });
+    } catch (error) {
+      logError(request, `Fetch error: ${error.message}`);
+      return new Response("Internal Server Error", { status: 500 });
     }
-
-    response = new Response(original_text, {
-      status,
-      headers: new_response_headers,
-    })
-  }
-  return response
-}
-
-async function replace_response_text(response, upstream_domain, host_name) {
-  let text = await response.text()
-
-  var i, j
-  for (i in replace_dict) {
-    j = replace_dict[i]
-    if (i == '$upstream') {
-      i = upstream_domain
-    } else if (i == '$custom_domain') {
-      i = host_name
-    }
-
-    if (j == '$upstream') {
-      j = upstream_domain
-    } else if (j == '$custom_domain') {
-      j = host_name
-    }
-
-    let re = new RegExp(i, 'g')
-    text = text.replace(re, j)
-  }
-
-  return text
-}
+  },
+};
